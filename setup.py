f8496f9627b7c6b71ca76ef83e1265609a3d7c53
Jialong.Li <jialong.li@cs.lth.se>
2025-11-19 16:05:42 +0000

    Revert "Fixed Problem 1-4 according to reviewer 2"
    This reverts commit f985201993bc86be9b6bad2ce8337809b27e2e47.

--- setup.py
+++ setup.py
@@ -21,8 +21,15 @@ setup(
     entry_points={
         'console_scripts': [
             'ros2quest = q2r_bringup.ros2quest:main',
-            'simulationInput = q2r_bringup.simulationInput:main',
+            'SImulationInpt = q2r_bringup.SImulationInpt:main',
             'CheckTCPconnection = q2r_bringup.CheckTCPconnection:main',
+            'set_target_point = q2r_bringup.set_target_point:main',
+            'go_to_xyz = q2r_bringup.go_to_xyz:main',
+            'send_goal_model = q2r_bringup.send_goal_model:main',
+            'send_goal = q2r_bringup.send_goal:main',
+            'fk_client = q2r_bringup.fk_client:main',
+            'Q2R_control_goal_left = q2r_bringup.Q2R_control_goal_left:main',
+            'Q2R_control_goal_right = q2r_bringup.Q2R_control_goal_right:main',
             'left_arm_controller = q2r_bringup.left_arm_controller:main',
             'right_arm_controller = q2r_bringup.right_arm_controller:main',
         ],
